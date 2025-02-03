f75c477c07eb917a2eb171dc31521572fa68a194
DC <nicalcoca@gmail.com>
2025-02-03 13:52:14 -0500

    Fixes for the deployment

--- backend/dist/auth/auth.controller.d.ts
+++ backend/dist/auth/auth.controller.d.ts
@@ -6,13 +6,9 @@ export declare class AuthController {
     private readonly csrfService;
     constructor(authService: AuthService, csrfService: CsrfService);
     getCsrfToken(res: Response): Response<any, Record<string, any>>;
-    login(email: string, password: string): Promise<{
-        accessToken: string;
-    }>;
+    login(email: string, password: string): Promise<import("../types/security").AccessToken>;
     validateToken(authHeader: string): Promise<any>;
-    finalizeRegistration(email: string, password: string): Promise<{
-        accessToken: string;
-    }>;
+    finalizeRegistration(email: string, password: string): Promise<import("../types/security").AccessToken>;
     setPassword(email: string, password: string): Promise<{
         message: string;
     }>;
--- frontend/src/components/navigation/phases.ts
+++ frontend/src/components/navigation/phases.ts
@@ -1,8 +1,8 @@
 // frontend/src/components/navigation/phases.ts
 export enum ProgressIncrements {
-    PROFILE = 100,     // 100/10 questions: 10
-    BFI = 100,      // 100/44 questions: 2.273
-    PRODUCT = 100   // 100/12 questions: 4.545
+    PROFILE = 10,     // 100/10 questions: 10
+    BFI = 2.273,      // 100/44 questions: 2.273
+    PRODUCT = 4.545   // 100/12 questions: 4.545
 }
 
 export const isValidPhase = (phase: string): phase is keyof typeof ProgressIncrements => {
