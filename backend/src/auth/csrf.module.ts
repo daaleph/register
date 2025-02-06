f7723db9e227e1af7f211ca2837bff6fb885f81c
DC <nicalcoca@gmail.com>
2025-02-05 19:58:55 -0500

    Changes in structure

--- backend/src/auth/csrf.module.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/csrf/csrf.module.ts
-import { Module } from '@nestjs/common';
-import { CsrfService } from './csrf.service';
-
-@Module({
-  providers: [CsrfService],
-  exports: [CsrfService],
-})
-export class CsrfModule {}
