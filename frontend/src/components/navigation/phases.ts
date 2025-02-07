c822e0c70d8bb4bd6c655e9b8f81130b7f307441
DC <nicalcoca@gmail.com>
2025-02-07 18:53:42 -0500

    Adjustments for horizontal items

--- frontend/src/components/navigation/phases.ts
+++ frontend/src/components/navigation/phases.ts
@@ -1,8 +1,8 @@
 // frontend/src/components/navigation/phases.ts
 export enum ProgressIncrements {
-    PROFILE = 10, 
-    BFI = 100/44, 
-    PRODUCT = 100/22
+    PROFILE = 51,      // 10 
+    BFI = 52,      // 100/44
+    PRODUCT = 53   // 100/22
 }
 
 export const isValidPhase = (phase: string): phase is keyof typeof ProgressIncrements => {
